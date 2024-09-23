252f6f9a742154655ced56b9a8b835a2b0d7d08d
chandkaurati <chandkaurati@outlook.com>
2024-09-23 11:33:16 +0530

    solved one debounce interview quesion

--- optimization/index.js
+++ optimization/index.js
@@ -16,3 +16,59 @@ const debounce = (fn, time) => {
 
 const debounceCallAPI = debounce(handleInput, 1000);
 inputEl.addEventListener("input", debounceCallAPI);
+
+
+
+
+// throtling
+
+
+async function  getPosts(params) {
+  const data = await fetch("https://dummyjson.com/products/categories")
+  console.log(data)
+}
+
+
+function useThrotling(fn, delay){
+  let timer = null
+  return function(){
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+        fn()
+    }, delay);
+  }
+}
+
+const fetchtProducts = useThrotling(getPosts, 400)
+
+document.addEventListener("scroll", ()=>{
+  fetchtProducts()
+})
+
+const btnEL = document.querySelector(".increment-btn")
+const pressedBtn =  document.querySelector(".increment-pressed")
+const triggerBtn = document.querySelector(".increment-triggerd")
+
+let pressCounter = 0;
+let triggerCounter = 0;
+
+function triggerIncrement(){
+  triggerBtn.innerHTML = ++triggerCounter;
+}
+
+function incrementDebounce(fn, delay){
+  let timer;
+  return function(){
+    clearTimeout(timer)
+    timer = setTimeout(() => {
+       triggerIncrement()
+    }, delay);
+  }
+}
+
+const debouncedTrigger = incrementDebounce(getPosts, 1000)
+btnEL.addEventListener("click", ()=>{
+  pressedBtn.innerHTML = ++pressCounter
+   debouncedTrigger()
+}) 
+
