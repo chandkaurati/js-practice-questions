b2b9affefd0cfc101bfbcb182f9806d02be52c14
chandkaurati <chandkaurati@outlook.com>
2024-09-27 13:48:24 +0530

    leared throtling

--- optimization/index.js
+++ optimization/index.js
@@ -1,6 +1,7 @@
 const inputEl = document.querySelector("input");
 
-const handleInput = (e) => {
+const handleInput =function (e) {
+  console.log("form the outer" , this.name);
   console.log(e.target.value);
 };
 
@@ -14,61 +15,111 @@ const debounce = (fn, time) => {
   };
 };
 
-const debounceCallAPI = debounce(handleInput, 1000);
-inputEl.addEventListener("input", debounceCallAPI);
+const myThrottletow = (cb, delay) => {
+  let firstCall = true;
+  let timer;
+  return function (...args) {
+    // console.log(this.name)
+    if (firstCall) {
+      cb.apply(this, args);
+      firstCall = false;
+      return;
+    }
+    if (timer) return;
+    timer = setTimeout(() => {
+      timer = null;
+      cb.apply(this, args);
+    }, delay);
+  };
+};
 
-
-
+const debounceCallAPI = myThrottletow(handleInput, 400);
+inputEl.addEventListener("input", (e) => {
+  debounceCallAPI.call({ name: "Chaand" }, e);
+});
 
 // throtling
 
-
-async function  getPosts(params) {
-  const data = await fetch("https://dummyjson.com/products/categories")
-  console.log(data)
+async function getPosts(params) {
+  const data = await fetch("https://dummyjson.com/products/categories");
+  console.log(data);
 }
 
-
-function useThrotling(fn, delay){
-  let timer = null
-  return function(){
-    clearTimeout(timer)
+function useThrotling(fn, delay) {
+  let timer = null;
+  return function () {
+    clearTimeout(timer);
     timer = setTimeout(() => {
-        fn()
+      fn();
     }, delay);
-  }
+  };
 }
 
-const fetchtProducts = useThrotling(getPosts, 400)
+// const fetchtProducts = useThrotling(getPosts, 400);
 
-document.addEventListener("scroll", ()=>{
-  fetchtProducts()
-})
+// document.addEventListener("scroll", () => {
+//   fetchtProducts();
+// });
 
-const btnEL = document.querySelector(".increment-btn")
-const pressedBtn =  document.querySelector(".increment-pressed")
-const triggerBtn = document.querySelector(".increment-triggerd")
+// debounce implementatiton
+
+// we can user loadsh debounce and throttle also
+const btnEL = document.querySelector(".increment-btn");
+const pressedBtn = document.querySelector(".increment-pressed");
+const triggerBtn = document.querySelector(".increment-triggerd");
 
 let pressCounter = 0;
 let triggerCounter = 0;
 
-function triggerIncrement(){
+function triggerIncrement() {
   triggerBtn.innerHTML = ++triggerCounter;
 }
 
-function incrementDebounce(fn, delay){
+function incrementDebounce(fn, delay) {
   let timer;
-  return function(){
-    clearTimeout(timer)
+  return function () {
+    clearTimeout(timer);
     timer = setTimeout(() => {
-       triggerIncrement()
+      triggerIncrement();
     }, delay);
-  }
+  };
 }
 
-const debouncedTrigger = incrementDebounce(getPosts, 1000)
-btnEL.addEventListener("click", ()=>{
-  pressedBtn.innerHTML = ++pressCounter
-   debouncedTrigger()
-}) 
+const debouncedTrigger = incrementDebounce(getPosts, 800);
+btnEL.addEventListener("click", () => {
+  pressedBtn.innerHTML = ++pressCounter;
+  debouncedTrigger();
+});
 
+// throttle implementation
+
+const throttleButtonElement = document.querySelector(".throtle-btn");
+const throttlePressdEL = document.querySelector(".throttle-pressed");
+const throttleTriggerEL = document.querySelector(".throttle-triggerd");
+
+let throttlePressCount = 0;
+let throttleTriggerCount = 0;
+
+function incrementThrottle() {
+  throttleTriggerEL.innerHTML = ++throttleTriggerCount;
+}
+
+const myThrottle = (cb, delay) => {
+  let timer;
+  return function (...args) {
+    if (timer) return;
+    timer = setTimeout(() => {
+      timer = null;
+      cb(...args);
+    }, delay);
+  };
+};
+
+const optimizedFn = myThrottle(incrementThrottle, 1000);
+
+throttleButtonElement.addEventListener("click", () => {
+  throttlePressdEL.innerHTML = ++throttlePressCount;
+  optimizedFn();
+});
+
+console.log("HI");
